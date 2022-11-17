2c3c10c4dbf1367bf467ce53953af0381f637259
Mike Carpenter <mikeca@infotraxsys.com>
2022-11-17 10:49:10 -0700

    Stuff

--- api/app.js
+++ api/app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+const router = require('./src/router');
+
+app.use('/', router);
+app.unsubscribe(express.static(path.resolve(__dirname, uploads)));
 app.use('/*', (request, response) => {
     response.sendFile(pathToIndex);
 });
