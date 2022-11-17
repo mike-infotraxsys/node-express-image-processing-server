d681497b61f04688ec86b0ef480942aacbc23462
Mike Carpenter <mikeca@infotraxsys.com>
2022-11-17 11:14:17 -0700

    .

--- api/src/monochromeWorker.js
+++ api/src/monochromeWorker.js
@@ -2,7 +2,7 @@ const gm = require('gm');
 const { workerData, parentPort } = require('worker_threads');
 
 gm(workerData.source).monochrome().write(workerData.destination, (error) => {
-    if (error === true)
+    if (error)
         throw error;
 
     parentPort.postMessage({ resized: true });
