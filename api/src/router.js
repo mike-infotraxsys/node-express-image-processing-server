b7faeee7fb8cdda2629ff3a80f30b6e52ae0d72a
Mike Carpenter <mikeca@infotraxsys.com>
2022-11-17 11:25:04 -0700

    .

--- api/src/router.js
+++ api/src/router.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
-const photoPath = path.resolve(__dirname, '../../client.photo-viewer.html');
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const router = Router();
 
@@ -10,21 +11,24 @@ const filename = (request, file, callback) => {
     callback(null, file.originalname);
 };
 
-const storage = multer.diskStorage({ destination: 'api/uploads/', filename: filename});
+const storage = multer.diskStorage({ 
+    destination: 'api/uploads/',
+    filename: filename
+});
 
 const fileFilter = (request, file, callback) => {
-    if (file.mimetype !== 'image/png')
-    {
+    if (file.mimetype !== 'image/png') {
         request.fileValidationError = "Wrong file type";
         callback(null, false, new Error("Wrong file type"));
-    }
-    else
-    {
+    } else {
         callback(null, true);
     }
 };
 
-const upload = multer({ fileFilter, storage});
+const upload = multer({
+    fileFilter,
+    storage
+});
 
 router.post('/upload', upload.single('photo'), (request, response) => {
     if (request.fileValidationError)
