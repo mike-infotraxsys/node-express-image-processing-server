f76bc3027fe33e9d76a1f4b3755268d5661c5e43
Mike Carpenter <mikeca@infotraxsys.com>
2022-11-17 11:13:05 -0700

    .

--- api/src/router.js
+++ api/src/router.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
 const multer = require('multer');
+const path = require('path');
+
+const photoPath = path.resolve(__dirname, '../../client.photo-viewer.html');
 
 const router = Router();
 
@@ -28,6 +31,10 @@ router.post('/upload', upload.single('photo'), (request, response) => {
         return response.status(400).json({error: request.fileValidationError});
     else
         return response.status(201).json({success: true});
+});
+
+router.get('/photo-viewer', (request, response) => {
+    response.sendFile(photoPath);
 });
 
 module.exports = router;
