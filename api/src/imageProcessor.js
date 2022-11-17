a48f0bd33cd347f07249fd12a081fca84c76ed28
Mike Carpenter <mikeca@infotraxsys.com>
2022-11-17 12:07:50 -0700

    .

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -6,7 +6,7 @@ const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
 
 const uploadPathResolver = (filename) => { 
     return path.resolve(__dirname, '../uploads', filename);
-};
+}
 
 const imageProcessor = (filename) => { 
     const sourcePath = uploadPathResolver(filename);
